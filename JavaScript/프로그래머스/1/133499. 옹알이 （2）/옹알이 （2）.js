91a1e975e99eb9896f244c6203419407e258704e
CDR4733 <166963977+CDR4733@users.noreply.github.com>
2024-06-24 09:55:59 +0900

    Update 옹알이 （2）.js

--- JavaScript/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ JavaScript/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -2,38 +2,16 @@ function solution(babbling) {
     var answer = 0;
     // 1. 가능한 발음들은 ""로 바뀌게 된다. 발음을 못하면 문자열이 남는다.
     for(let i = 0; i < babbling.length; i++){
-        do {
-            if (babbling[i].includes("ayaaya")){
-                break;
-            }
-            if (babbling[i].includes("aya")){
-                babbling[i] = babbling[i].replace("aya", " ");
-            }
-        } while (babbling[i].includes("aya"));
-        do {
-            if (babbling[i].includes("yeye")){
-                break;
-            }
-            if (babbling[i].includes("ye")){
-                babbling[i] = babbling[i].replace("ye", " ");
-            }
-        } while (babbling[i].includes("ye"));
-        do {
-            if (babbling[i].includes("woowoo")){
-                break;
-            }
-            if (babbling[i].includes("woo")){
-                babbling[i] = babbling[i].replace("woo", " ");
-            }
-        } while (babbling[i].includes("woo"));
-        do {
-            if (babbling[i].includes("mama")){
-                break;
-            }
-            if (babbling[i].includes("ma")){
-                babbling[i] = babbling[i].replace("ma", " ");
-            }
-        } while (babbling[i].includes("ma"));                    
+        for (let c = 0; c < can.length; c++) {
+            do {
+                if (babbling[i].includes(can[c] + can[c])) {
+                    break;
+                }
+                if (babbling[i].includes(can[c])) {
+                    babbling[i] = babbling[i].replace(can[c], " ");
+                }
+            } while (babbling[i].includes(can[c]));
+        }         
     }
     
     // 2. 공백으로만 이루어진 문자열을 ""로 바꿔주기
