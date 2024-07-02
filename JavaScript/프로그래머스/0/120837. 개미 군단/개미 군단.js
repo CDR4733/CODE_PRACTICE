1ee388b20f6f835c919b646ab73b85536254454c
CDR4733 <166963977+CDR4733@users.noreply.github.com>
2024-07-02 09:56:36 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ JavaScript/프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,12 +1,12 @@
 function solution(hp) {
     var answer = 0;    
-    // 개미의 종류 입력
+    // 1. 개미의 종류 입력
     var ants = [5, 3, 1];
-    // 최소병력 구성해서 -> answer
+    // 2. 최소병력 구성해서 -> answer
     for (let i = 0; i < ants.length; i++) {
         answer += parseInt(hp / ants[i]);
         hp -= ants[i] * parseInt(hp / ants[i]);
     }    
-    // 결괏값 반환
+    // 3. 결괏값 반환
     return answer;
 }
