ca623e55420f160df107b54074b3911ab9639bbe
CDR4733 <166963977+CDR4733@users.noreply.github.com>
2024-07-09 07:00:12 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 33.3 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ JavaScript/프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,7 +1,3 @@
 function solution(dot) {    
-    if (dot[0] > 0) {
-        return dot[1] > 0 ? 1 : 4;
-    } else {
-        return dot[1] > 0 ? 2 : 3;
-    }
+    return dot[0] > 0 ? (dot[1] > 0 ? 1 : 4) : (dot[1] > 0 ? 2 : 3);
 }
