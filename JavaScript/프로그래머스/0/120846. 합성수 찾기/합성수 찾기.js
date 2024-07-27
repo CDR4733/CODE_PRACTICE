3d84694195636bb8cc75afb46ea2721863ba036f
CDR4733 <166963977+CDR4733@users.noreply.github.com>
2024-07-27 15:33:44 +0900

    [level 0] Title: 합성수 찾기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ JavaScript/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,16 +1,12 @@
 function solution(n) {
-    var answer = 0;
-    for(let i = 1; i < n + 1; i++) {
-       let count = 0;
-       for(let j = 1; j < i + 1; j++) {
-           if (i % j == 0) {
-               count++;
-           }
-           if (count >= 3) {
-               answer++;
-               break;
-           }
-       }
+  var answer = 0;
+  for (let i = 1; i < n + 1; i++) {
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) {
+        answer++;
+        break;
+      }
     }
-    return answer;
+  }
+  return answer;
 }
